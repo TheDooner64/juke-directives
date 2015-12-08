ae0e2979d4a6d0c69c1bda4f69a68797aa3e271e
John L <john.m.lenehan@gmail.com>
2015-12-08 18:34:19 -0500

    Final work for the day

---
+++ browser/js/directives/doubleDirective.js
@@ -0,0 +1,17 @@
+app.directive('doubleClick', function() {
+
+    return {
+
+        restrict: "A",
+        scope: {
+            doubleClick: "&"
+        },
+        link: function(scope, element, attrs) {
+
+            element.on("dblclick", function() {
+                scope.doubleClick();
+            })
+
+        }
+    }
+})
